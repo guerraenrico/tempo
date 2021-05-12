fc10300d13f12fe81cd84cc7edeaac0a9adffd1a
guerraenrico <portapozze@libero.it>
2021-05-12 23:16:39 +0200

    deps: compose 1.0.0-beta06, hilt 2.35.1, firebase 28.0.0, gradle 4.2.0

--- buildSrc/src/main/java/com/enricog/tempo/buildsrc/dependencies.kt
+++ buildSrc/src/main/java/com/enricog/tempo/buildsrc/dependencies.kt
@@ -49,7 +49,7 @@ object Libs {
         }
 
         object Compose {
-            const val version = "1.0.0-beta05"
+            const val version = "1.0.0-beta06"
 
             const val runtime = "androidx.compose.runtime:runtime:$version"
             const val foundation = "androidx.compose.foundation:foundation:$version"
@@ -63,11 +63,11 @@ object Libs {
             const val test = "androidx.compose.ui:ui-test:$version"
             const val uiTest = "androidx.compose.ui:ui-test-junit4:$version"
 
-            const val activity = "androidx.activity:activity-compose:1.3.0-alpha06"
+            const val activity = "androidx.activity:activity-compose:1.3.0-alpha07"
 
             const val navigation = "androidx.navigation:navigation-compose:1.0.0-alpha10"
             const val viewModel = "androidx.lifecycle:lifecycle-viewmodel-compose:1.0.0-alpha04"
-            const val constraintLayout = "androidx.constraintlayout:constraintlayout-compose:1.0.0-alpha05"
+            const val constraintLayout = "androidx.constraintlayout:constraintlayout-compose:1.0.0-alpha06"
         }
     }
 
@@ -83,7 +83,7 @@ object Libs {
     }
 
     object Hilt {
-        const val androidVersion = "2.35"
+        const val androidVersion = "2.35.1"
         private const val version = "1.0.0-beta01"
 
         const val android = "com.google.dagger:hilt-android:$androidVersion"
@@ -116,7 +116,7 @@ object Libs {
     }
 
     object Firebase {
-        private const val version = "27.1.0"
+        private const val version = "28.0.0"
 
         const val bom = "com.google.firebase:firebase-bom:$version"
         const val crashlytics = "com.google.firebase:firebase-crashlytics-ktx"
