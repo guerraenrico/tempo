0654e8771c14e7557e4cceb6507dc7a4a09942d4
guerraenrico <portapozze@libero.it>
2021-12-31 22:48:47 +0100

    Slide in and out start routine button

--- features/routines/src/main/java/com/enricog/routines/detail/summary/ui_components/RoutineSummaryScene.kt
+++ features/routines/src/main/java/com/enricog/routines/detail/summary/ui_components/RoutineSummaryScene.kt
@@ -3,6 +3,8 @@ package com.enricog.routines.detail.summary.ui_components
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.layout.Arrangement.spacedBy
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
@@ -12,7 +14,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
@@ -20,7 +21,6 @@ import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -185,8 +185,8 @@ internal fun RoutineSummaryScene(
 
         AnimatedVisibility(
             visible = !isDragging,
-            enter = fadeIn(),
-            exit = fadeOut(),
+            enter = slideInVertically(initialOffsetY = { it / 2 }),
+            exit = slideOutVertically(targetOffsetY = { it / 2 }),
             modifier = Modifier.align(Alignment.BottomCenter)
         ) {
             TempoIconButton(
