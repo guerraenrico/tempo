59b7d16d16488b387f6f0364f562642ee2ef1133
guerraenrico <portapozze@libero.it>
2022-02-27 18:14:17 +0100

    Replace mockk with fake in routines segment detail

--- features/routines/src/test/java/com/enricog/features/routines/detail/segment/SegmentViewModelTest.kt
+++ features/routines/src/test/java/com/enricog/features/routines/detail/segment/SegmentViewModelTest.kt
@@ -1,177 +1,145 @@
 package com.enricog.features.routines.detail.segment
 
 import androidx.lifecycle.SavedStateHandle
+import app.cash.turbine.test
 import com.enricog.core.coroutines.testing.CoroutineRule
+import com.enricog.data.local.testing.FakeStore
 import com.enricog.data.routines.api.entities.Routine
 import com.enricog.data.routines.api.entities.Segment
 import com.enricog.data.routines.api.entities.TimeType
+import com.enricog.data.routines.testing.FakeRoutineDataSource
 import com.enricog.data.routines.testing.entities.EMPTY
 import com.enricog.entities.asID
 import com.enricog.entities.seconds
+import com.enricog.features.routines.R
 import com.enricog.features.routines.detail.segment.models.SegmentField
-import com.enricog.features.routines.detail.segment.models.SegmentFieldError
-import com.enricog.features.routines.detail.segment.models.SegmentState
 import com.enricog.features.routines.detail.segment.models.SegmentViewState
 import com.enricog.features.routines.detail.segment.usecase.SegmentUseCase
 import com.enricog.features.routines.navigation.RoutinesNavigationActions
 import com.enricog.navigation.testing.FakeNavigator
-import io.mockk.coEvery
-import io.mockk.coVerify
-import io.mockk.every
-import io.mockk.mockk
-import io.mockk.verify
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import kotlin.test.assertEquals
+import kotlin.time.ExperimentalTime
 
+@OptIn(ExperimentalTime::class)
 class SegmentViewModelTest {
 
     @get:Rule
     val coroutineRule = CoroutineRule()
 
-    private val navigator = FakeNavigator()
-
-    private val converter: SegmentStateConverter = mockk()
-    private val reducer: SegmentReducer = mockk()
-    private val segmentUseCase: SegmentUseCase = mockk(relaxUnitFun = true)
-    private val validator: SegmentValidator = mockk()
-    private val savedStateHandle = SavedStateHandle(
-        mapOf(
-            "routineId" to 1L,
-            "segmentId" to 2L
-        )
+    private val segment = Segment.EMPTY.copy(
+        id = 2.asID,
+        name = "Segment Name",
+        time = 30.seconds,
+        type = TimeType.TIMER
+    )
+    private val routine = Routine.EMPTY.copy(
+        id = 1.asID,
+        segments = listOf(segment)
     )
 
-    @Before
-    fun setup() {
-        coEvery { segmentUseCase.get(routineId = any()) } returns Routine.EMPTY
-        // needed because right now mockk doesn't fully support value classes see https://github.com/mockk/mockk/issues/152
-        coEvery { segmentUseCase.save(routine = any(), segment = any()) } returns Unit
-        coEvery { converter.convert(state = any()) } returns SegmentViewState.Idle
-        every {
-            reducer.setup(routine = any(), segmentId = any())
-        } returns SegmentState.Data(
-            routine = Routine.EMPTY,
-            segment = Segment.EMPTY,
-            errors = emptyMap(),
-            timeTypes = emptyList()
-        )
-    }
+    private val navigator = FakeNavigator()
+    private val savedStateHandle = SavedStateHandle(
+        mapOf("routineId" to 1L, "segmentId" to 2L)
+    )
 
     @Test
-    fun `should get routine and set segment on load `() = coroutineRule {
-        coEvery { segmentUseCase.get(routineId = any()) } returns Routine.EMPTY
-        buildSut()
+    fun `should setup segment on load `() = coroutineRule {
+        val expected = SegmentViewState.Data(
+            segment = segment,
+            errors = emptyMap(),
+            timeTypes = listOf(TimeType.TIMER, TimeType.REST, TimeType.STOPWATCH)
+        )
 
-        coVerify {
-            segmentUseCase.get(routineId = 1.asID)
-            reducer.setup(routine = Routine.EMPTY, segmentId = 2.asID)
-        }
+        val sut = buildSut()
+
+        sut.viewState.test { assertEquals(expected, expectItem()) }
     }
 
     @Test
     fun `should update segment name onSegmentNameTextChange`() = coroutineRule {
-        val state = SegmentState.Data(
-            routine = Routine.EMPTY,
-            segment = Segment.EMPTY,
+        val expected = SegmentViewState.Data(
+            segment = segment.copy(name = "Segment Name Modified"),
             errors = emptyMap(),
-            timeTypes = emptyList()
+            timeTypes = listOf(TimeType.TIMER, TimeType.REST, TimeType.STOPWATCH)
         )
-        every { reducer.setup(routine = any(), segmentId = any()) } returns state
-        every { reducer.updateSegmentName(state = any(), text = any()) } returns state
         val sut = buildSut()
 
-        sut.onSegmentNameTextChange(text = "name")
+        sut.onSegmentNameTextChange(text = "Segment Name Modified")
 
-        verify { reducer.updateSegmentName(state = state, text = "name") }
+        sut.viewState.test { assertEquals(expected, expectItem()) }
     }
 
     @Test
     fun `should update segment time onSegmentTimeChange`() = coroutineRule {
-        val state = SegmentState.Data(
-            routine = Routine.EMPTY,
-            segment = Segment.EMPTY,
+        val expected = SegmentViewState.Data(
+            segment = segment.copy(time = 10.seconds),
             errors = emptyMap(),
-            timeTypes = emptyList()
+            timeTypes = listOf(TimeType.TIMER, TimeType.REST, TimeType.STOPWATCH)
         )
-        every { reducer.setup(routine = any(), segmentId = any()) } returns state
-        every { reducer.updateSegmentTime(state = any(), seconds = 10.seconds) } returns state
         val sut = buildSut()
 
         sut.onSegmentTimeChange(seconds = 10.seconds)
 
-        verify { reducer.updateSegmentTime(state = state, seconds = 10.seconds) }
+        sut.viewState.test { assertEquals(expected, expectItem()) }
     }
 
     @Test
     fun `should update segment type onSegmentTypeChange`() = coroutineRule {
-        val state = SegmentState.Data(
-            routine = Routine.EMPTY,
-            segment = Segment.EMPTY,
+        val expected = SegmentViewState.Data(
+            segment = segment.copy(type = TimeType.STOPWATCH, time = 0.seconds),
             errors = emptyMap(),
-            timeTypes = emptyList()
+            timeTypes = listOf(TimeType.TIMER, TimeType.REST, TimeType.STOPWATCH)
         )
-        every { reducer.setup(routine = any(), segmentId = any()) } returns state
-        every { reducer.updateSegmentTimeType(state = any(), timeType = any()) } returns state
         val sut = buildSut()
 
         sut.onSegmentTypeChange(timeType = TimeType.STOPWATCH)
 
-        verify { reducer.updateSegmentTimeType(state = state, timeType = TimeType.STOPWATCH) }
+        sut.viewState.test { assertEquals(expected, expectItem()) }
     }
 
     @Test
-    fun `should apply errors when segment has errors onSegmentConfirmed`() = coroutineRule {
-        val state = SegmentState.Data(
-            routine = Routine.EMPTY,
-            segment = Segment.EMPTY,
-            errors = emptyMap(),
-            timeTypes = emptyList()
+    fun `should show errors when segment has errors onSegmentConfirmed`() = coroutineRule {
+        val expected = SegmentViewState.Data(
+            segment = segment.copy(name = ""),
+            errors = mapOf(SegmentField.Name to R.string.field_error_message_segment_name_blank),
+            timeTypes = listOf(TimeType.TIMER, TimeType.REST, TimeType.STOPWATCH)
         )
-        val errors = mapOf<SegmentField, SegmentFieldError>(
-            SegmentField.TimeInSeconds to SegmentFieldError.InvalidSegmentTime
-        )
-        every { reducer.setup(routine = any(), segmentId = any()) } returns state
-        every { reducer.applySegmentErrors(state = any(), errors = any()) } returns state
-        every { validator.validate(any()) } returns errors
         val sut = buildSut()
+        sut.onSegmentNameTextChange(text = "")
 
         sut.onSegmentConfirmed()
 
         navigator.assertNoActions()
-        verify { reducer.applySegmentErrors(state = state, errors = errors) }
-        coVerify(exactly = 0) { segmentUseCase.save(routine = any(), segment = any()) }
+        sut.viewState.test { assertEquals(expected, expectItem()) }
     }
 
     @Test
     fun `should save and go back when segment has no errors onSegmentConfirmed`() = coroutineRule {
-        val state = SegmentState.Data(
-            routine = Routine.EMPTY,
-            segment = Segment.EMPTY,
-            errors = emptyMap(),
-            timeTypes = emptyList()
+        val store = FakeStore(listOf(routine))
+        val expected = routine.copy(
+            segments = listOf(segment.copy(name = "Segment Name Modified"))
         )
-        every { reducer.setup(routine = any(), segmentId = any()) } returns state
-        every { validator.validate(any()) } returns emptyMap()
-        val sut = buildSut()
+        val sut = buildSut(store)
+        sut.onSegmentNameTextChange(text = "Segment Name Modified")
 
         sut.onSegmentConfirmed()
 
         navigator.assertGoBack()
-        coVerify {
-            segmentUseCase.save(routine = Routine.EMPTY, segment = Segment.EMPTY)
-        }
-        verify(exactly = 0) { reducer.applySegmentErrors(state = any(), errors = any()) }
+        assertEquals(expected, store.get().first())
     }
 
-    private fun buildSut(): SegmentViewModel {
+    private fun buildSut(store: FakeStore<List<Routine>> = FakeStore(listOf(routine))): SegmentViewModel {
         return SegmentViewModel(
             savedStateHandle = savedStateHandle,
             dispatchers = coroutineRule.dispatchers,
-            converter = converter,
-            reducer = reducer,
-            segmentUseCase = segmentUseCase,
-            validator = validator,
+            converter = SegmentStateConverter(),
+            reducer = SegmentReducer(),
+            segmentUseCase = SegmentUseCase(
+                routineDataSource = FakeRoutineDataSource(store)
+            ),
+            validator = SegmentValidator(),
             navigationActions = RoutinesNavigationActions(navigator)
         )
     }
