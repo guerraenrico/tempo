205c84ec5d4f2861d6ceffccaa9b8076475535ca
guerraenrico <portapozze@libero.it>
2022-04-25 23:39:44 +0200

    Use SegmnetInputs on save

--- features/routines/src/main/java/com/enricog/features/routines/detail/segment/SegmentViewModel.kt
+++ features/routines/src/main/java/com/enricog/features/routines/detail/segment/SegmentViewModel.kt
@@ -6,16 +6,15 @@ import com.enricog.base.viewmodel.BaseViewModel
 import com.enricog.base.viewmodel.ViewModelConfiguration
 import com.enricog.core.coroutines.dispatchers.CoroutineDispatchers
 import com.enricog.core.coroutines.job.autoCancelableJob
-import com.enricog.entities.Seconds
 import com.enricog.data.routines.api.entities.Routine
 import com.enricog.data.routines.api.entities.Segment
 import com.enricog.data.routines.api.entities.TimeType
-import com.enricog.navigation.api.routes.SegmentRoute
-import com.enricog.navigation.api.routes.SegmentRouteInput
 import com.enricog.features.routines.detail.segment.models.SegmentState
 import com.enricog.features.routines.detail.segment.models.SegmentViewState
 import com.enricog.features.routines.detail.segment.usecase.SegmentUseCase
 import com.enricog.features.routines.navigation.RoutinesNavigationActions
+import com.enricog.navigation.api.routes.SegmentRoute
+import com.enricog.navigation.api.routes.SegmentRouteInput
 import com.enricog.ui.components.textField.TimeText
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -59,7 +58,7 @@ internal class SegmentViewModel @Inject constructor(
 
     fun onSegmentTimeChange(text: TimeText) {
         updateStateWhen<SegmentState.Data> { stateData ->
-            reducer.updateSegmentTime(state = stateData, seconds = seconds)
+            reducer.updateSegmentTime(state = stateData, text = text)
         }
     }
 
@@ -71,9 +70,12 @@ internal class SegmentViewModel @Inject constructor(
 
     fun onSegmentConfirmed() {
         runWhen<SegmentState.Data> { stateData ->
-            val errors = validator.validate(segment = stateData.segment)
+            val errors = validator.validate(inputs = stateData.inputs)
             if (errors.isEmpty()) {
-                save(routine = stateData.routine, segment = stateData.segment)
+                save(
+                    routine = stateData.routine,
+                    segment = stateData.inputs.mergeToSegment(stateData.segment)
+                )
             } else {
                 updateStateWhen<SegmentState.Data> {
                     reducer.applySegmentErrors(state = it, errors = errors)
--- features/routines/src/main/java/com/enricog/features/routines/detail/segment/models/SegmentInputs.kt
+++ features/routines/src/main/java/com/enricog/features/routines/detail/segment/models/SegmentInputs.kt
@@ -1,5 +1,6 @@
 package com.enricog.features.routines.detail.segment.models
 
+import com.enricog.data.routines.api.entities.Segment
 import com.enricog.data.routines.api.entities.TimeType
 import com.enricog.ui.components.textField.TimeText
 
@@ -7,4 +8,13 @@ internal data class SegmentInputs(
     val name: String,
     val time: TimeText,
     val type: TimeType
-)
+) {
+
+    fun mergeToSegment(segment: Segment): Segment {
+        return segment.copy(
+            name = name,
+            time = time.toSeconds(),
+            type = type
+        )
+    }
+}
