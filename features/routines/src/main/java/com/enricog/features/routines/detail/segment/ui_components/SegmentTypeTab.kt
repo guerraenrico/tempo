2f7260655c56965a15cc67f805b2df26df521238
guerraenrico <portapozze@libero.it>
2022-07-07 23:16:27 +0200

    Use TempoText in SegmentTypeTab

--- features/routines/src/main/java/com/enricog/features/routines/detail/segment/ui_components/SegmentTypeTab.kt
+++ features/routines/src/main/java/com/enricog/features/routines/detail/segment/ui_components/SegmentTypeTab.kt
@@ -4,16 +4,18 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.enricog.data.routines.api.entities.TimeType
 import com.enricog.features.routines.detail.ui.time_type.text
+import com.enricog.ui.components.text.TempoText
 import com.enricog.ui.theme.TempoTheme
 import com.enricog.ui.theme.white
 
@@ -37,11 +39,13 @@ internal fun SegmentTypeTab(
             },
         contentAlignment = Alignment.Center
     ) {
-        Text(
-            text = value.text(),
-            color = white,
-            fontWeight = FontWeight.Bold,
-            fontSize = 14.sp,
+        TempoText(
+            text = AnnotatedString(value.text()),
+            style = TextStyle(
+                color = white,
+                fontWeight = FontWeight.Bold,
+                fontSize = 14.sp,
+            ),
             modifier = Modifier.padding(
                 horizontal = TempoTheme.dimensions.spaceM,
                 vertical = TempoTheme.dimensions.spaceXS
