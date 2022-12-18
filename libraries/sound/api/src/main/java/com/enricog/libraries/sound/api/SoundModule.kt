02e1e67b9d6861af328e73d142c2c1f491a902b1
guerraenrico <portapozze@libero.it>
2022-12-18 10:43:42 +0100

    Avoind injecting a singletone SoundPlayer

--- libraries/sound/api/src/main/java/com/enricog/libraries/sound/api/SoundModule.kt
+++ libraries/sound/api/src/main/java/com/enricog/libraries/sound/api/SoundModule.kt
@@ -4,13 +4,11 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 internal abstract class SoundModule {
 
     @Binds
-    @Singleton
     abstract fun provideSoundPlayer(impl: SoundPlayerImpl): SoundPlayer
 }
