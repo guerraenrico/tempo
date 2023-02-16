e4985280e46b757f81a1cbff2cefc93044384136
guerraenrico <portapozze@libero.it>
2023-02-16 23:25:04 +0100

    Replace migration to v3 from RENAME COLUM to manual table copy

--- data/local/database/src/main/java/com/enricog/data/local/database/migrations/MigrationFrom2To3.kt
+++ data/local/database/src/main/java/com/enricog/data/local/database/migrations/MigrationFrom2To3.kt
@@ -6,6 +6,19 @@ import androidx.sqlite.db.SupportSQLiteDatabase
 internal object MigrationFrom2To3 : Migration(2, 3) {
 
     override fun migrate(database: SupportSQLiteDatabase) {
-        database.execSQL("ALTER TABLE Routines RENAME COLUMN 'startTimeOffsetInSeconds' TO 'preparationTimeInSeconds';")
+        database.beginTransaction()
+        try {
+            database.execSQL("CREATE TABLE RoutinesTemp (routineId INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name TEXT NOT NULL, preparationTimeInSeconds INTEGER NOT NULL, createdAt TEXT NOT NULL, updatedAt TEXT NOT NULL, rank TEXT NOT NULL)")
+
+            database.execSQL("INSERT INTO RoutinesTemp(routineId, name, preparationTimeInSeconds, createdAt, updatedAt, rank) SELECT routineId, name, startTimeOffsetInSeconds, createdAt, updatedAt, rank FROM Routines")
+
+            database.execSQL("DROP TABLE Routines")
+
+            database.execSQL("ALTER TABLE RoutinesTemp RENAME TO Routines")
+
+            database.setTransactionSuccessful()
+        } finally {
+            database.endTransaction()
+        }
     }
 }
